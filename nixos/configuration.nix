b8977ce770510ba226e2210a038cf31068ddbecb
aovoono <dobre.aleksa@icloud.com>
2023-12-27 21:35:57 +0100

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,7 +10,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
   boot.extraModprobeConfig = ''
       options hid_apple fnmode=1
   '';
